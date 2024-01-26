95b8ee79a3a4adce1d5f48a365d4b1480e934fc2
Nguyen Van Manh <nguyenvanmanh2001it1@gmail.com>
2024-01-26 16:35:08 +0700

    update

---
+++ app.js
@@ -0,0 +1,8 @@
+ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+
+ScrollReveal().reveal('#nav-menu li', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+
