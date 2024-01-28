6292368555540773ed3f34d0fa61e102ced7f4b8
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-01-29 00:23:36 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+// view cv 
+if (localStorage.getItem('view_cv') === null) {
+    localStorage.setItem('view_cv', '1');
+} else {
+    let viewCount = parseInt(localStorage.getItem('view_cv')) + 1;
+    localStorage.setItem('view_cv', viewCount.toString());
+}
+let currentViewCount = localStorage.getItem('view_cv');
+document.getElementById('number_view').textContent = currentViewCount;
+
+// Hiển thị hiệu ứng chạy số từ 0 đến 'view_cv'
+let displayCount = 0;
+let increment = 1; // Bước tăng giá trị, có thể điều chỉnh
+
+let intervalId = setInterval(function () {
+    document.getElementById('number_view').textContent = displayCount;
+    displayCount += increment;
+
+    if (displayCount > currentViewCount) {
+        clearInterval(intervalId);
+        document.getElementById('number_view').textContent = currentViewCount;
+    }
+}, 50); // Thời gian cập nhật, có thể điều chỉnh
+
+let intervalId2 = setInterval(function () {
+    document.getElementById('number_view2').textContent = displayCount;
+    displayCount += increment;
+
+    if (displayCount > currentViewCount) {
+        clearInterval(intervalId);
+        document.getElementById('number_view2').textContent = currentViewCount;
+    }
+}, 50); // Thời gian cập nhật, có thể điều chỉnh
+
+
 // scroll top 
 const btnScrollToTop = document.querySelector(".btnScrollToTop");
 btnScrollToTop.addEventListener("click", e => {
@@ -29,4 +64,6 @@ ScrollReveal().reveal('#objective' , { delay: 500, origin: 'bottom', interval: 2
 ScrollReveal().reveal('#education' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
 ScrollReveal().reveal('#work , #work-detail li p' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
 ScrollReveal().reveal('#skills , #skills ul li' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#interests ' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#other div li ' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
     
