31bda5800beb62f401bc23e62eefad2a961979d2
Nguyen Van Manh <nguyenvanmanh2001it1@gmail.com>
2024-01-29 15:46:17 +0700

    update opacity

--- app.js
+++ app.js
@@ -40,14 +40,26 @@ let intervalId2 = setInterval(function () {
 // scroll top 
 const btnScrollToTop = document.querySelector(".btnScrollToTop");
 btnScrollToTop.addEventListener("click", e => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth"
-  });
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
 });
-window.addEventListener('scroll', e => {
-  btnScrollToTop.style.display = window.scrollY > 20 ? 'block' : 'none';
+// window.addEventListener('scroll', e => {
+//   btnScrollToTop.style.display = window.scrollY > 20 ? 'block' : 'none';
+// });
+window.addEventListener('scroll', () => {
+    const scrollThreshold = 20;
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > scrollThreshold) {
+        // Scroll đủ lớn, hiển thị nút và đặt opacity là 1
+        btnScrollToTop.style.opacity = '1';
+    } else {
+        // Scroll không đủ, ẩn nút và đặt opacity là 0
+        btnScrollToTop.style.opacity = '0';
+    }
 });
 
 // ScrollReveal
@@ -57,17 +69,16 @@ ScrollReveal({
     delay: 400
 });
 
-ScrollReveal().reveal('#about', { delay: 600, origin: 'bottom' });       
+ScrollReveal().reveal('#about', { delay: 600, origin: 'bottom' });
 ScrollReveal().reveal('#nav-menu li ', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#about div' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#inner-about p , #additional > p , #additional li ' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#introduction' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#infor-detail li p' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#contact , #contact-detail li' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#objective' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#education' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#work , #work-detail li p' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#skills , #skills ul li' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#interests ' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-ScrollReveal().reveal('#other div li ' , { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
-    
+ScrollReveal().reveal('#about div', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#inner-about p , #additional > p , #additional li ', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#introduction', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#infor-detail li p', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#contact , #contact-detail li', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#objective', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#education', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#work , #work-detail li p', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#skills , #skills ul li', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#interests ', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
+ScrollReveal().reveal('#other div li ', { delay: 500, origin: 'bottom', interval: 200, distance: '300px' });
